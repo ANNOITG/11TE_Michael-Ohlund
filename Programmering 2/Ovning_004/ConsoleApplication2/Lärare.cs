17be5ea2bb9d7bffd36f432fa421a3a9c3e8d846
Nepeta <Micckkkeee@hotmail.com>
2013-09-24 11:43:03 +0200

    Lade till grunderna i projektet
    Skapar klasserna Person -> Elev, Lärare. Med mera...

---
+++ Programmering 2/Ovning_004/ConsoleApplication2/Lärare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Lärare : Person
+    {
+        public Lärare()
+        {
+        
+        }
+        public Lärare(string namn, string adress, string pnr, string telenr)
+        {
+            setNamn(namn);
+            setAdress(adress);
+            setPnr(pnr);
+            setTelenr(telenr);
+        }
+    }
+}
---
+++ Programmering 2/Ovning_004/ConsoleApplication2/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Person
+    {
+        private string namn;
+        private string pnr;
+        private string telenr;
+        private string adress;
+
+        public string getNamn() { return namn; }
+        public void setNamn(string namn) { this.namn = namn; }
+
+        public string getPnr() { return pnr; }
+        public void setPnr(string personnummer) { this.pnr = personnummer; }
+
+        public string getTelenr() { return telenr; }
+        public void setTelenr(string telenr) { this.telenr = telenr; }
+
+        public string getAdress() { return adress; }
+        public void setAdress(string adress) { this.adress = adress; }
+
+
+    }
+}
