f18418ed4eebfc419235e3202fdd4933590d254f
Nepeta <Micckkkeee@hotmail.com>
2013-12-02 12:04:15 +0100

    I has le done part 6
    Meow?

--- Webbserver/MvcMusicStore/MvcMusicStore/Global.asax.cs
+++ Webbserver/MvcMusicStore/MvcMusicStore/Global.asax.cs
@@ -33,11 +33,9 @@ namespace MvcMusicStore
 
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(
+                new MvcMusicStore.Models.SampleData());
             AreaRegistration.RegisterAllAreas();
-
-            // Use LocalDB for Entity Framework by default
-            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
-
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
         }
